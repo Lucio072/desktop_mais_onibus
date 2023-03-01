f171be9e7e459f80c8feabd43dc856fbf46c42e1
gabriel <gabrieljbs@gmail.com>
2023-03-01 15:03:19 -0300

    primeiro commit

---
+++ projeto/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+/* import { PagesComponent } from './app/pages/pages.component'; */
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    /* PagesComponent, */
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ projeto/src/app/pages/pages.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sapp-page',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.scss']
+})
+export class PagesComponent {
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
