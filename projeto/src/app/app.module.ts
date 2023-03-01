5d60dec8d567cd6a14320ff52744d553244b5bd1
gabriel <gabrieljbs@gmail.com>
2023-03-01 16:29:19 -0300

    header

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HeaderComponent } from './template/header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RegisterComponent } from './pages/register/register.component';
     /* PagesComponent, */
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
